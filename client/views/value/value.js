e0071cc48b571dddaeaab43d60bafc4d8b86c27e
David D. Boling <daviddboling@gmail.com>
2014-09-13 12:16:24 -0500

    Chart.js added, need to massage chart data

--- client/views/value/value.js
+++ client/views/value/value.js
@@ -1,3 +1,5 @@
+/* global Chart */
+
 (function(){
   'use strict';
 
@@ -10,7 +12,39 @@
     Value.getData('908 Archer St', 'Nashville', 'TN', '37203').then(function(response){
       $scope.zestimate = response.data.zestimate;
       $scope.demographics = response.data.demographics;
+
+      //will add data later
+      createBar();
     });
+
+    function createBar(){
+      var data = {
+        labels: ['Home', 'City', 'Nation'],
+        datasets: [
+          {
+            fillColor: 'rgba(220,220,220,0.5)',
+            strokeColor: 'rgba(220,220,220,0.8)',
+            highlightFill: 'rgba(220,220,220,0.75)',
+            highlightStroke: 'rgba(220,220,220,1)',
+            data: [65, 59, 80, 81, 56, 55, 40]
+          },
+          {
+            fillColor: 'rgba(151,187,205,0.5)',
+            strokeColor: 'rgba(151,187,205,0.8)',
+            highlightFill: 'rgba(151,187,205,0.75)',
+            highlightStroke: 'rgba(151,187,205,1)',
+            data: [28, 48, 40, 19, 86, 27, 90]
+          }
+        ]
+      },
+      ctx = document.getElementById('chart').getContext('2d');
+      new Chart(ctx).Bar(data);
+    }
+
+
+
+
+
 
   }]);
 })();
