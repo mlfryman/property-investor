eba17d44b388e24616c218aa7feb36142dc6811e
Melanie Fryman <melanie@frymanet.com>
2014-09-12 23:00:15 -0500

    included tab placeholders in home.jade

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,7 +1,21 @@
+/* jshint camelcase:false */
+
 (function(){
   'use strict';
 
   angular.module('prop')
-  .controller('HomeCtrl', [function(){
+  .controller('HomeCtrl', ['$scope', 'Home', function($scope, Home){
+    $scope.title = 'Property Investor';
+
+    // DELETE ME! - this is for testing only.
+    $scope.location = {};
+    $scope.locations = [];
+    $scope.search = function(){
+      $scope.locations.push($scope.location);
+      $scope.location = {};
+    };
+    // END DELETE
+
+// Last brackets
   }]);
 })();
